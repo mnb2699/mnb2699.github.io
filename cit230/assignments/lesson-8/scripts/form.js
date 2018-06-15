3c6866fd77e22a24f2074e17cce45205f385df4e
Meghan B <bre16016@byui.edu>
2018-06-15 10:13:09 -0600

    Finished!

--- cit230/assignments/lesson-8/scripts/form.js
+++ cit230/assignments/lesson-8/scripts/form.js
@@ -1,7 +1,3 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
-
-slider.oninput = function(){
-    output.innerHTML = this.value;
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
 }
