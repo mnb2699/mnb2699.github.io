350bd61bb8a2f3f9a1c3aa446b9ca06cf0f91d65
Meghan B <bre16016@byui.edu>
2018-07-18 17:11:05 -0600

    it is finished

--- cit230/bike/scripts/services.js
+++ cit230/bike/scripts/services.js
@@ -1,4 +1,28 @@
-*part of your json
+"use strict";
+var w3 = {};  
+w3.includeHTML = function(cb) {
+  var z, i, elmnt, file, xhttp;
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          elmnt.innerHTML = this.responseText + elmnt.innerHTML;
+          elmnt.removeAttribute("w3-include-html");
+          w3.includeHTML(cb);
+        }
+      }      
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      return;
+    }
+  }
+  if (cb) cb();
+};
+
 
 var requesrtURL = 'data/sercises.json';
 
@@ -11,5 +35,3 @@ request.send();
 request.onload = function() {
     var services = request.response;
 }
-
-use the w3 include 
